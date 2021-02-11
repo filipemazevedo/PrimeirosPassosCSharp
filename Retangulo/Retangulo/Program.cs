2d6bd02d0319c4e0cd3e1ee7b5827cfa1c88445c
Filipe Azevedo <filipedemenezes@hotmail.com>
2021-02-11 11:29:46 -0300

    Add files via upload

---
+++ Retangulo/Retangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+namespace ExercicioRetangulo
+{
+    /* Fazer um programa para ler os valores da largura e altura de um retângulo. Em
+    seguida, mostrar na tela o valor de sua área, perímetro e diagonal. Usar uma classe
+    como mostrado no projeto ao lado.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret;
+            ret = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e a altura do retângulo: ");
+            ret.L = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            ret.H = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Área = " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perímetro = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + ret.Diagonal().ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Retangulo/Retangulo/Retangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioRetangulo
+{
+    class Retangulo
+    {
+        public Double L, H;
+
+        public double Area()
+        {
+            return L * H;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (L + H);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(L,2) + Math.Pow(H,2));
+        }
+    }
+}
