0d998874e6b398a51c077d039539257609f5f04e
Filipe Azevedo <filipedemenezes@hotmail.com>
2021-04-07 09:59:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+namespace ExercicioContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n): ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("");
+            Console.Write("Entre com um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine());
+            conta.Deposito(quantia);
+            Console.Write("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("");
+            Console.Write("Entre com um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine());
+            conta.Saque(quantia);
+            Console.Write("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+
+        }
+    }
+}
